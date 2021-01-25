d1941736b343ee54aabcc26a81ebb7471eeeb81f
lamhuynhb779 <lamhuynhb779@gmail.com>
2021-01-25 22:22:58 +0700

    add explain

--- CreationalPatterns/Builder/php/CarBuilder.php
+++ CreationalPatterns/Builder/php/CarBuilder.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Separate the construction of a complex object from its representation so that 
+ * the same construction process can create different representations.
+ * 
+ * Parse a complex representation, create one of several targets.
+ */
+
 include('Car.php');
 include('Engine.php');
 include('SeatBelt.php');
